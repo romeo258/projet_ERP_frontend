a65d99df74b4442e7fa9be966086ae822cc530cf
Romeo <temgouaromeo@gmail.com>
2024-07-24 19:59:30 +0100

    ajout backend railway1

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   development: true,
   // API_BASE_URL: 'http://192.168.56.1:8080'
-  API_BASE_URL: 'https://projeterpbackend-production.up.railway.app/'
+  API_BASE_URL: 'https://projeterpbackend-production.up.railway.app'
 };
