4744a7bf2c31b07e1f5ec196ec66e0c6a848eaa1
Romeo <temgouaromeo@gmail.com>
2024-07-24 19:38:57 +0100

    ajout backend railway

--- src/app/interceptor/cache.interceptor.ts
+++ src/app/interceptor/cache.interceptor.ts
@@ -17,7 +17,7 @@ export class CacheInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> | Observable<HttpResponse<unknown>> {
     if (request.url.includes('verify') || request.url.includes('login') || request.url.includes('register')
       || request.url.includes('refresh') || request.url.includes('resetpassword')
-      || request.url.includes('verify') || request.url.includes('new/password')) {
+      || request.url.includes('verify') || request.url.includes('new/password') || request.url.includes('stats') ) {
       return next.handle(request);
     }
     if (request.method !== 'GET' || request.url.includes('download')) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  API_BASE_URL: 'http://192.168.56.1:8080'
+  API_BASE_URL: 'https://projeterpbackend-production.up.railway.app/'
 };
